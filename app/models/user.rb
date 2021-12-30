a2f2b08aebbf97deac3b374c7bb9bc4e240bc1d1
thr3eisl4nd <thre3islands@gmail.com>
2021-12-30 21:12:53 +0900

    エラーメッセージの表示と他
    エラーメッセージの表示
    バリデーションで設定してたエラー文の削除、一部修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: /@+/ }
+  validates :email, presence: true, format: { with: /@+/ }
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください', confirmation: true
-  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
-  validates :phonetic_family_name, presence: true, format: { with: /\A[ァ-ヴー]+\z/, meesage: "全角カタカナのみで入力して下さい" }
-  validates :phonetic_first_name, presence: true, format: { with: /\A[ァ-ヴー]+\z/, meesage: "全角カタカナのみで入力して下さい" }
+  validates_format_of :password, with: PASSWORD_REGEX, confirmation: true
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, }
+  validates :phonetic_family_name, presence: true, format: { with: /\A[ァ-ヴー]+\z/, }
+  validates :phonetic_first_name, presence: true, format: { with: /\A[ァ-ヴー]+\z/, }
   validates :birth_date, presence: true
 end
