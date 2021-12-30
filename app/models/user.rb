10cab8b6e1243d905ff461b5735e33e21ef7463e
thr3eisl4nd <thre3islands@gmail.com>
2021-12-30 12:43:50 +0900

    バリデーション変更0.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください', confirmation: true
   validates family_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
   validates first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
-  validates phonetic_family_name, presence: true format: { with: /A\[ァ-ヴー]+\z/u, meesage: "全角カタカナのみで入力して下さい" }
-  validates phonetic_first_name, presence: true　format: { with: /A\[ァ-ヴー]+\z/u, meesage: "全角カタカナのみで入力して下さい" }
+  validates phonetic_family_name, presence: true, format: { with: /A\[ァ-ヴー]+\z/u, meesage: "全角カタカナのみで入力して下さい" }
+  validates phonetic_first_name, presence: true, format: { with: /A\[ァ-ヴー]+\z/u, meesage: "全角カタカナのみで入力して下さい" }
   validates birth_date, presence: true
 end
