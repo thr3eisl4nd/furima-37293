5a4b9d8b39108d6078cb3c4b48e87ce3351c5ff1
thr3eisl4nd <thre3islands@gmail.com>
2022-01-04 20:58:58 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,12 @@
 class Item < ApplicationRecord
+
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :shipment_days
+  belongs_to :shipment_source
+
 end
