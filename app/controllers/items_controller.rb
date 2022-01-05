d6f9d0a089979489f1b8e4ffabd2f87d788a05f5
thr3eisl4nd <thre3islands@gmail.com>
2022-01-05 12:56:26 +0900

    商品出品ページの修正
    ActiveHashの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,14 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.find(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :delivery_id, :shipment_days_id, :shipment_source_id)
+  end
+
 end
