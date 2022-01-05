ff5c3e5bcbeb223ec90146439ebd8f12b3bbde4f
thr3eisl4nd <thre3islands@gmail.com>
2022-01-05 12:16:42 +0900

    商品出品ページを表示
    formにメソッドを渡して表示

--- app/models/shipment_source.rb
+++ app/models/shipment_source.rb
@@ -48,7 +48,8 @@ class Shipment_source < ActiveHash::Base
     { id: 46, name: '宮崎県' }, 
     { id: 47, name: '鹿児島県' }, 
     { id: 48, name: '沖縄県' }
-
+  ]
+  
     include ActiveHash::Associations
     has_many :items
 
