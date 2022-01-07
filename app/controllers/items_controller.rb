aa17d6debaa487297cd91ef20c3028102998fff6
thr3eisl4nd <thre3islands@gmail.com>
2022-01-07 17:19:10 +0900

    レビュー後の修正
    「出品者以外は削除できない」ように実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,7 +38,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
+    if @item.user != current_user.id
+      @item.destroy
       redirect_to root_path
     end
   end
