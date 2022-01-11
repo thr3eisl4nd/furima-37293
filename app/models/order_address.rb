49dc6ae481a1e5d55132b1cf0f61e63f20474314
thr3eisl4nd <thre3islands@gmail.com>
2022-01-11 18:25:07 +0900

    Formオブジェクトの作成
    order_addressファイルを作成、クラスを定義
    Active::Modelを呼び出し、form_withメソッドに対応する機能とバリデーションを行う機能
    attr_accessorを用いて、保存したい複数のテーブルのカラム名を全て扱えるようにする
    バリデーションの処理を書く
    データをテーブルに保存する処理を書く

---
+++ app/models/order_address.rb
@@ -0,0 +1,16 @@
+class OrderAddress
+  include ActivieModel::Model
+  attr_accessor :zipcode, :shipment_source_id, :address1, :address2, :building_name, :phone_number, :order_id, :item_id, :user_id
+
+  with_options: true do
+    validates :zipcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :phone_number, format: {with: /\A0^[0-9]{10,11}\z/, message: "is invalid. Input half-width numbers"}
+    validates :address1, :address2, :order_id, :item_id, :user_id
+  end
+  validates :shipment_source_id, numericality: {other_than: 1, message: "can't be blank"}
+
+  def save
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Address.create(zipcode: zipcode, shipment_source_id: shipment_source_id, address1: address1, address2: address2, phone_number: phone_number, order_id: order.id)
+  end
+end
