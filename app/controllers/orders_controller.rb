5a9142a4adf42f6df5fcd860ffce8024efe6ef33
thr3eisl4nd <thre3islands@gmail.com>
2022-01-11 22:03:58 +0900

    購入のためのフォームを実装
    ordersのindexビューにurlオプションに指定のパスを記述
    modelオプションにインスタンス変数を定義する

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,8 @@
 class OrdersController < ApplicationController
   def index
-    @order = Order.new(order_params)
+    @order_address = OrderAddress.new
   end
   def create
-  end
-
-  private
-  
-  def order_params
-    params.require(:order_address).permit(:zipcode, :shipment_source_id, :address1, :address2, :building_name, :phone_number, :order_id).merge(item_id: [:item_id], user_id: current_user.id)
   end
 
 end
--- db/migrate/20220111093139_create_addresses.rb
+++ db/migrate/20220111093139_create_addresses.rb
@@ -1,7 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-
+      t.string :zipcode, null: false
+      t.integer :shipment_source_id, null: false
+      t.string :address1, null: false
+      t.string :address2, null: false
+      t.string :building_name
+      t.string :phone_number, null: false
+      t.references :order, foreign_key: true
       t.timestamps
     end
   end
