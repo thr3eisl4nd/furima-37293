2f49a28085a68fc27833af8a61731f4f1ceec536
thr3eisl4nd <thre3islands@gmail.com>
2022-01-12 22:19:43 +0900

    公開鍵と共にカード情報をPAY.JP側へ送る
    ブラウザのカード情報取得
    公開鍵と共にカード情報をPAY.JP側へ送る
    トークンが発行されたことを確認
    トークンをサーバーサイドへ送る

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,4 +1,5 @@
 const pay = () => {
+  Payjp.setPublicKey("pk_test_a99912c9e733dc98f8092f4f");
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
@@ -11,7 +12,25 @@ const pay = () => {
       exp_month: formData.get("order_address[exp_month]"),
       exp_year: `20${formData.get("order_address[exp_year]")}`,
       cvc: formData.get("order_address[cvc]"),
+
     };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token)
+        const renderDom = document.getElementById("charge-form")
+        const token0bj = `<input value=${token} name='token' type="hidden">`;
+        renderDom.insertAdjacentHTML("beforeend", token0bj);
+      }
+
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
+    });
   });
 };
 
