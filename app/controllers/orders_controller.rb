495e39b4532b62a1f793301e9fa15c30c198244b
thr3eisl4nd <thre3islands@gmail.com>
2022-01-12 17:20:45 +0900

    フォームの情報を保存できるようにする/エラー文の表示
    createアクションで処理が呼び出され、意図するパラメーターが送信されてるか確認
    送られてきたパラメーターを保存する処理の記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,21 @@
 class OrdersController < ApplicationController
   def index
     @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
   end
   def create
+    @order_address = OrderAddress.new(order_item_params)
+    @item = Item.find(params[:item_id])
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
+  private
+    def order_item_params
+      params.require(:order_address).permit(:zipcode, :shipment_source_id, :address1, :address2, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
+    end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,11 +3,11 @@ class OrderAddress
   attr_accessor :zipcode, :shipment_source_id, :address1, :address2, :building_name, :phone_number, :order_id, :item_id, :user_id
 
   with_options presence: true do
-    validates :zipcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :phone_number, format: {with: /\A0^[0-9]{10,11}\z/, message: "is invalid. Input half-width numbers"}
-    validates :address1, :address2, :order_id, :item_id, :user_id
+    validates :zipcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly"}
+    validates :address1, :address2, :item_id, :user_id
+    validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, message: "Input only number"}
   end
-  validates :shipment_source_id, numericality: {other_than: 1, message: "can't be blank"}
+  validates :shipment_source_id, numericality: {other_than: 1, message: "Select"}
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
