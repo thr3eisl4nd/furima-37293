bfa7491ab4631734a31be58bd5f2ac5fbd8f919c
thr3eisl4nd <thre3islands@gmail.com>
2022-01-13 18:41:09 +0900

    購入済み商品の条件分岐など
    「ログイン状態の場合でも、自身が出品していない売却済み商品の商品購入ページへ遷移しようとすると、トップページに遷移すること」
    「ログイン状態の場合でも、自身が出品した商品の商品購入ページに遷移しようとすると、商品の販売状況に関わらずトップページに遷移すること」
    「ログアウト状態の場合は、商品購入ページに遷移しようとすると、商品の販売状況に関わらずログインページに遷移すること」
    
    売却済みの商品は、画像上に「sold out」の文字が表示されるようになっていること（商品一覧機能）。
    売却済みの商品は、画像上に「sold out」の文字が表示されるようになっていること（商品詳細機能）。
    ログイン状態の場合でも、売却済みの商品には、「商品の編集」「削除」「購入画面に進む」ボタンが表示されないこと（商品詳細機能）。
    ログイン状態の場合でも、自身が出品した売却済み商品の商品情報編集ページへ遷移しようとすると、トップページに遷移すること（商品情報編集機能）。

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,15 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :item_find_id, only: [:index, :create]
+
   def index
     @order_address = OrderAddress.new
-    @item = Item.find(params[:item_id])
+    if @item.order != nil || @item.user_id == current_user.id
+      redirect_to root_path
+    end
   end
   def create
     @order_address = OrderAddress.new(order_item_params)
-    @item = Item.find(params[:item_id])
     if @order_address.valid?
       Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
       Payjp::Charge.create(
@@ -21,6 +25,11 @@ class OrdersController < ApplicationController
   end
 
   private
+
+    def item_find_id
+      @item = Item.find(params[:item_id])
+    end
+
     def order_item_params
       params.require(:order_address).permit(:zipcode, :shipment_source_id, :address1, :address2, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
     end
